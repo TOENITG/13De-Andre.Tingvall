0b7f7b8a0b04492e685f925e8e0a7b89e2b00d7a
Crowbalt <Andre@o11cnf025cpn7.learnet.se>
2014-10-13 13:27:04 +0200

    Added comments

--- Uppgift1/src/uppgift1/mainBlah.java
+++ Uppgift1/src/uppgift1/mainBlah.java
@@ -6,26 +6,26 @@ public class mainBlah {
 
 	public static void main(String[] args) {
 		
-		String fNamn = JOptionPane.showInputDialog("Förnamn:");
-		String eNamn = JOptionPane.showInputDialog("Efternamn:");
-		String pNamn = JOptionPane.showInputDialog("Personnummer:");
-		String aNamn = JOptionPane.showInputDialog("Adress:");
+		String namn = JOptionPane.showInputDialog("Namn:"); //Frågar efter namn
+		String pNamn = JOptionPane.showInputDialog("Personnummer:"); // -||- personnummer
+		String aNamn = JOptionPane.showInputDialog("Adress:"); // -||- adress
 		
-		int[] pNum = {Integer.parseInt(pNamn.substring(2,4)),Integer.parseInt(pNamn.substring(4,6))};
+		int[] pNum = {Integer.parseInt(pNamn.substring(2,4)),Integer.parseInt(pNamn.substring(4,6))}; 
+		//hämtar ut dag och månad ur personnummret
 		
-		String[] manad = {	"Januari","Februari","Mars",
+		String[] manad = {	"Januari","Februari","Mars", //Namnet på alla månader som ska användas
 							"April","Maj","Juni",
-							"Juli","Augusti","September",
-							"Oktober","November","December"};
+							"Juli","Augusti","September", 
+							"Oktober","November","December"}; 
 		
-		JOptionPane.showMessageDialog(null, (	"Välkommen " + fNamn + " " + eNamn + ",\n" +
+		JOptionPane.showMessageDialog(null, (	"Välkommen " + namn +  ",\n" + // skriver texten
 												"då du fyller den " + pNum[1] + " " + manad[pNum[0]-1] + 
 												" kommer vi komma till dig,\n" +
 												aNamn + " och fira dig.\n" + 
 												"Detta är ett automatiskt meddelande."),
-												"Message to puny human named " + fNamn, 1);
+												"Message to puny human named " + namn, 1); // texten i toppen av dialogrutan och typ av meddelande
 		
-		System.exit(0);
+		System.exit(0); // ser till att programmet verkeligen stänger ner sig även om det görs automatiskt
 	}
 	
 }
--- Uppgift1/src/uppgift2/mainFiskFis.java
+++ Uppgift1/src/uppgift2/mainFiskFis.java
@@ -6,14 +6,14 @@ public class mainFiskFis {
 
 	public static void main(String[] args) {
 		
-		String[] aStr = new String[5]; 
-		String outP = "";
+		String[] aStr = new String[5];  //sparar all ny text i en position i arrayen
+		String outP = ""; //strängen som skrivs ut
 		
-		for(int i = 0; i < 5; i++){
-			aStr[i] = JOptionPane.showInputDialog("Inmatning " + (i + 1));
-			outP += " " + aStr[i];
+		for(int i = 0; i < 5; i++){ //loopen som gör allt
+			aStr[i] = JOptionPane.showInputDialog("Inmatning " + (i + 1)); // frågar efter nästa sträng
+			outP += " " + aStr[i]; //för över strängen till outP
 			
-			JOptionPane.showMessageDialog( null, outP, outP, JOptionPane.INFORMATION_MESSAGE);
+			JOptionPane.showMessageDialog( null, outP, outP, JOptionPane.INFORMATION_MESSAGE); //skriver ut strängen
 		}
 		
 		System.exit(0);
