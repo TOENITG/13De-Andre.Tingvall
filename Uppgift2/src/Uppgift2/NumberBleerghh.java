dbbd42d15576cbd5e648ea58e667cbe6ecb8cc9d
Crowbalt <Andre@o112ce12302r0.learnet.se>
2014-11-03 14:41:55 +0100

    uppg2 JOption
    Använder JOption istället för konsolen

--- Uppgift2/src/Uppgift2/NumberBleerghh.java
+++ Uppgift2/src/Uppgift2/NumberBleerghh.java
@@ -1,36 +1,39 @@
 package Uppgift2;
 
-import java.util.Scanner;
+import javax.swing.JOptionPane;
 
 public class NumberBleerghh {
 
 
 	static double[] inP = {0, 0};
-	static Scanner scan = new Scanner(System.in);
+	
+	public static void message(String s){
+		JOptionPane.showMessageDialog(null, s);
+	}
 	
 	public static void input(int i){ //ser till at inmatningen är korrekt
 
 		while(true){ 
 			try{
-				inP[i] = Double.valueOf((scan.next().toString().replaceAll(",", ".")));
+				inP[i] = Double.valueOf((JOptionPane.showInputDialog("").toString().replaceAll(",", ".")));
 				break;
 			}catch(java.lang.NumberFormatException e){
-				System.out.println("Felaktig input, försök igen med siffror");
+				message("Felaktig input, försök igen med siffror");
 			}
 		}
 	}
 	public static void main(String[] args) {
 		
 		@SuppressWarnings("resource")
-		String[] typ = {"Addition; ", "Subtraktion; ","Multiplikation; ","Division; "};
+		String[] typ = {"Addition: ", "Subtraktion: ","Multiplikation: ","Division: "};
 		
-		System.out.println("Ge mig 2 tal, skriv nu första talet!");
+		message("Ge mig 2 tal, skriv nu första talet!");
 		
 		input(0);
 		
 		for(int i = 0; i < 4; i++){ //Antalet par som ska köras igenom
 			
-			System.out.println("Skriv ytterligare ett tal!");
+			message("Skriv ytterligare ett tal!");
 			
 			input(1);
 			
@@ -49,11 +52,11 @@ public class NumberBleerghh {
 				break;
 			}
 			
-			System.out.println(typ[i] + inP[0]);
+			message(typ[i] + inP[0]);
 			
 		}
 		
-		System.out.println("Bra jobbat! Programmet är slut");
+		message("Bra jobbat! Programmet är slut");
 		
 		System.exit(0);
 	}
